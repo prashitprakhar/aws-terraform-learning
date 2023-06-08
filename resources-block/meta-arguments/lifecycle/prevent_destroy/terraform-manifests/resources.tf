d03d9d0f686bc733ca5af47be93a9b14f05dc0cf
Prashit Prakhar <prashitprakhar@yahoo.com>
2023-06-09 02:16:31 +0530

    Meta-arguments: lifecycle

---
+++ resources-block/meta-arguments/lifecycle/prevent_destroy/terraform-manifests/resources.tf
@@ -0,0 +1,16 @@
+# EC2 Resource config
+
+resource "aws_instance" "my-ec2-instance" {
+  ami           = "ami-0607784b46cbe5816"
+  instance_type = "t2.micro"
+  #   availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1b"
+  tags = {
+    "Name" = "web"
+  }
+
+  # Meta Arguments: lifecycle
+  #   lifecycle {
+  #     prevent_destroy = true
+  #   }
+}
